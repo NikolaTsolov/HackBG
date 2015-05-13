ed6bf8cd44ad7a5552d8cf6baa147cfcda4fd524
NikolaTsolov <NikolaTsolov@users.noreply.github.com>
2015-05-13 17:28:00 +0300

    Delete ContSubstrings.py

--- Week1/TheRealDeal/ContSubstrings.py
+++
@@ -1,8 +0,0 @@
-def count_substrings(heystack, needle):
-    return heystack.count(needle)
-
-def main():
-    print(count_substrings("This is somthing", "is"))
-
-if __name__ == '__main__':
-    main()
